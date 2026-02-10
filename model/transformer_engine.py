e5d840f680cdbfcd2a21825e7bb46ff4b2a3c2fe
Hiba250 <hiba@example.com>
2026-02-10 13:00:00 +0530

    Implement generate_sql inference method

--- model/transformer_engine.py
+++ model/transformer_engine.py
@@ -4,3 +4,11 @@ class TransformerNL2SQLEngine:
     def __init__(self, model_path):
         self.tokenizer = T5Tokenizer.from_pretrained(model_path)
         self.model = T5ForConditionalGeneration.from_pretrained(model_path)
+        self.model.eval()
+
+    def generate_sql(self, question, table_name, columns):
+        schema = 'Table: ' + table_name + ' | Cols: ' + ', '.join(columns)
+        inp = 'translate English to SQL: ' + question + ' ' + schema
+        ids = self.tokenizer(inp, return_tensors='pt').input_ids
+        out = self.model.generate(ids, max_new_tokens=96)
+        return self.tokenizer.decode(out[0], skip_special_tokens=True)
