9c6a8584b2676fd9d907320226fed077cea3547e
Hiba250 <hiba@example.com>
2026-02-12 11:15:00 +0530

    Add batch_size and learning rate args to trainer

--- model/train.py
+++ model/train.py
@@ -1,12 +1,14 @@
 import argparse
 
 def train(args):
-    print('Training...')
+    print('bs=' + str(args.batch_size) + ' lr=' + str(args.lr))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', required=True)
+    parser.add_argument('--data_dir')
     parser.add_argument('--model', default='t5-small')
     parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--lr', type=float, default=3e-4)
     args = parser.parse_args()
     train(args)
