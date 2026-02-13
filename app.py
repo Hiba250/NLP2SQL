4232882a5e9c601ae092ce73ed3f373c2adca67f
Hiba250 <hiba@example.com>
2026-02-13 14:30:00 +0530

    Add Flask /generate endpoint

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'NLP2SQL'
 
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    question = data.get('question', '')
+    return jsonify({'sql': 'SELECT * FROM table -- ' + question})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
