35dfeff0cb908b946cb3ac1321943378f6e79ebb
Hiba250 <hiba@example.com>
2026-02-14 09:45:00 +0530

    Add sample database schemas dict

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
+SCHEMAS = {'employees': {'table': 'employees', 'columns': ['id','name','salary']}, 'products': {'table': 'products', 'columns': ['id','name','price']}}
 
 @app.route('/')
 def index():
@@ -7,9 +8,7 @@ def index():
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    data = request.get_json()
-    question = data.get('question', '')
-    return jsonify({'sql': 'SELECT * FROM table -- ' + question})
+    return jsonify({'sql': 'SELECT * FROM employees'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
