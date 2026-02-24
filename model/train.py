72c474a53bc4d85ca9c0f4239309f0310394ab04
Hiba250 <hiba@example.com>
2026-02-24 09:00:00 +0530

    Add gradient accumulation support to trainer

--- model/train.py
+++ model/train.py
@@ -1,14 +1,14 @@
 import argparse
 
 def train(args):
-    print('bs=' + str(args.batch_size) + ' lr=' + str(args.lr))
+    print('grad_accum=' + str(args.grad_accum))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir')
-    parser.add_argument('--model', default='t5-small')
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--lr', type=float, default=3e-4)
+    parser.add_argument('--grad_accum', type=int, default=1)
     args = parser.parse_args()
     train(args)
