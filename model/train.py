4fed730663c41582821e99b24d315245e8687127
Hiba250 <hiba@example.com>
2026-02-25 10:30:00 +0530

    Add warmup scheduler and cosine LR decay

--- model/train.py
+++ model/train.py
@@ -1,14 +1,16 @@
 import argparse
 
+def get_scheduler(optimizer, warmup_steps, total_steps):
+    from transformers import get_cosine_schedule_with_warmup
+    return get_cosine_schedule_with_warmup(optimizer, warmup_steps, total_steps)
+
 def train(args):
-    print('grad_accum=' + str(args.grad_accum))
+    print('Scheduler: cosine with warmup')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir')
     parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--lr', type=float, default=3e-4)
-    parser.add_argument('--grad_accum', type=int, default=1)
     args = parser.parse_args()
     train(args)
