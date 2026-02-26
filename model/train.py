64b9ce135bcf9b62a819fa7bf1f3fb6b594ee41b
Hiba250 <hiba@example.com>
2026-02-26 11:00:00 +0530

    Add FP16 mixed precision training flag

--- model/train.py
+++ model/train.py
@@ -1,16 +1,15 @@
 import argparse
 
-def get_scheduler(optimizer, warmup_steps, total_steps):
-    from transformers import get_cosine_schedule_with_warmup
-    return get_cosine_schedule_with_warmup(optimizer, warmup_steps, total_steps)
-
 def train(args):
-    print('Scheduler: cosine with warmup')
+    if args.fp16:
+        print('FP16 enabled')
+    print('LR=' + str(args.lr))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir')
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--lr', type=float, default=3e-4)
+    parser.add_argument('--fp16', action='store_true')
     args = parser.parse_args()
     train(args)
