75bb1d3df671d43cd47bff8c66b7e2ce17dcb96d
Hiba250 <hiba@example.com>
2026-02-27 14:15:00 +0530

    Add best model checkpoint saving logic

--- model/train.py
+++ model/train.py
@@ -1,15 +1,18 @@
-import argparse
+import argparse, os
+
+def save_best(model, path):
+    os.makedirs(path, exist_ok=True)
+    if hasattr(model, 'save_pretrained'):
+        model.save_pretrained(path)
+    print('Saved best model to ' + path)
 
 def train(args):
-    if args.fp16:
-        print('FP16 enabled')
-    print('LR=' + str(args.lr))
+    print('Training with checkpointing')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir')
+    parser.add_argument('--checkpoint_dir', default='checkpoints')
     parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--lr', type=float, default=3e-4)
-    parser.add_argument('--fp16', action='store_true')
     args = parser.parse_args()
     train(args)
