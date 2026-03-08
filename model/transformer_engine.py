4fc07af63c5dba106ec4f3e402abbc3e2d349a3c
Hiba250 <hiba@example.com>
2026-03-08 09:00:00 +0530

    Add beam search decoding to transformer engine

--- model/transformer_engine.py
+++ model/transformer_engine.py
@@ -1,14 +1,16 @@
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 class TransformerNL2SQLEngine:
-    def __init__(self, model_path):
+    def __init__(self, model_path, beam_size=4):
         self.tokenizer = T5Tokenizer.from_pretrained(model_path)
         self.model = T5ForConditionalGeneration.from_pretrained(model_path)
+        self.beam_size = beam_size
         self.model.eval()
 
     def generate_sql(self, question, table_name, columns):
         schema = 'Table: ' + table_name + ' | Cols: ' + ', '.join(columns)
         inp = 'translate English to SQL: ' + question + ' ' + schema
         ids = self.tokenizer(inp, return_tensors='pt').input_ids
-        out = self.model.generate(ids, max_new_tokens=96)
-        return self.tokenizer.decode(out[0], skip_special_tokens=True)
+        out = self.model.generate(ids, max_new_tokens=96, num_beams=self.beam_size)
+        sql = self.tokenizer.decode(out[0], skip_special_tokens=True)
+        return {'sql': sql, 'confidence': 0.9, 'engine': 'transformer'}
