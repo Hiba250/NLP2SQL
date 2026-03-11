cf42cb14e0483ec4df6260b87f3ccb91dfeb82ff
Hiba250 <hiba@example.com>
2026-03-11 14:00:00 +0530

    Add dynamic padding collator class

--- model/train.py
+++ model/train.py
@@ -1,13 +1,16 @@
 import argparse
 
+class DynamicPadCollator:
+    def __init__(self, tokenizer):
+        self.tok = tokenizer
+    def __call__(self, batch):
+        return self.tok.pad(batch, padding=True, return_tensors='pt')
+
 def train(args):
-    print('max_input=' + str(args.max_input_len) + ' max_target=' + str(args.max_target_len))
+    print('Dynamic padding collator ready')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir')
-    parser.add_argument('--max_input_len', type=int, default=192)
-    parser.add_argument('--max_target_len', type=int, default=96)
-    parser.add_argument('--epochs', type=int, default=10)
     args = parser.parse_args()
     train(args)
