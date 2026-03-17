dba27e54905dbf38e8134e9c8a4df793dbb01b36
Hiba250 <hiba@example.com>
2026-03-17 11:30:00 +0530

    Add step-level loss logging to training loop

--- model/train.py
+++ model/train.py
@@ -1,14 +1,16 @@
 import argparse
 
 def train(args):
-    for epoch in range(args.epochs):
-        if epoch % args.eval_every == 0:
-            print('Eval at epoch ' + str(epoch))
+    step_losses = []
+    for step in range(100):
+        loss = 1.0 / (step + 1)
+        if step % 10 == 0:
+            print('Step ' + str(step) + ': loss=' + str(round(loss, 4)))
+        step_losses.append(loss)
+    print('Done training')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir')
-    parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--eval_every', type=int, default=2)
     args = parser.parse_args()
     train(args)
