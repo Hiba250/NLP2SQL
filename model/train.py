faf272d5e023945314fe5b6e42cd411723e7e1fb
Hiba250 <hiba@example.com>
2026-03-26 09:30:00 +0530

    Add LR schedule graph generation

--- model/train.py
+++ model/train.py
@@ -1,17 +1,24 @@
-import argparse
+import argparse, os
+
+def plot_lr(lr_history, out_dir):
+    try:
+        import matplotlib.pyplot as plt
+        plt.plot(lr_history, label='LR')
+        plt.xlabel('Step')
+        plt.ylabel('LR')
+        plt.legend()
+        plt.savefig(os.path.join(out_dir, 'lr_schedule.png'))
+        plt.close()
+    except ImportError:
+        pass
 
 def train(args):
-    start_epoch = 0
-    if args.resume:
-        print('Resuming from ' + args.resume)
-        start_epoch = 3
-    for epoch in range(start_epoch, args.epochs):
-        print('Epoch ' + str(epoch))
+    lr_steps = [3e-4 * (i / 10) for i in range(10)]
+    plot_lr(lr_steps, args.checkpoint_dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir')
-    parser.add_argument('--resume', default=None)
-    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--checkpoint_dir', default='checkpoints')
     args = parser.parse_args()
     train(args)
